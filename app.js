58d748d73d6b0a85165218a219829f978e6d1b3c
Smaton1 <sam_maton@hotmail.com>
2014-09-23 12:52:55 +0100

    updated next button

--- app.js
+++ app.js
@@ -68,7 +68,7 @@
 
     $scope.next=function(){
       $scope.answer = ""
-      fetch_random($scope.countries)
+      fetch_random(countries)
         $http.get("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22" + $scope.randomcapital + "%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys.json").success(function(data){
       $scope.randomcapitaldata = data
       $scope.randomcapitaltemp = $scope.randomcapitaldata.query.results.channel.item.condition.temp
